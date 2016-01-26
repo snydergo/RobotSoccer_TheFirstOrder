e9e5fb1ef65627f4e98c9cdc0b1b9c8eb47bf40f
Troy Hinckley <t.macman@gmail.com>
2016-01-26 09:32:34 -0700

    Update ColorSegmentation.cpp

--- vision/ColorSegmentation.cpp
+++ vision/ColorSegmentation.cpp
@@ -12,30 +12,30 @@ using namespace std;
 
     if ( !cap.isOpened() )  // if not success, exit program
     {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
+        cout << "Cannot open the web cam" << endl;
+        return -1;
     }
 
     namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
-  int iLowH = 0;
- int iHighH = 179;
+    int iLowH = 0;
+    int iHighH = 179;
 
-  int iLowS = 0; 
- int iHighS = 255;
+    int iLowS = 0; 
+    int iHighS = 255;
 
-  int iLowV = 0;
- int iHighV = 255;
+    int iLowV = 0;
+    int iHighV = 255;
 
-  //Create trackbars in "Control" window
- cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
- cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+    //Create trackbars in "Control" window
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
-  cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
- cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
-  cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
- cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
     while (true)
     {
@@ -43,39 +43,39 @@ using namespace std;
 
         bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-         if (!bSuccess) //if not success, break loop
+        if (!bSuccess) //if not success, break loop
         {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
+            cout << "Cannot read a frame from video stream" << endl;
+            break;
         }
 
-    Mat imgHSV;
+        Mat imgHSV;
 
-   cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
  
-  Mat imgThresholded;
+        Mat imgThresholded;
 
-   inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-      
-  //morphological opening (remove small objects from the foreground)
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
-   //morphological closing (fill small holes in the foreground)
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        //morphological opening (remove small objects from the foreground)
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
 
-   imshow("Thresholded Image", imgThresholded); //show the thresholded image
-  imshow("Original", imgOriginal); //show the original image
+        //morphological closing (fill small holes in the foreground)
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+        imshow("Original", imgOriginal); //show the original image
 
         if (waitKey(10) == 27) //wait for 'esc' key press for 10ms. If 'esc' key is pressed, break loop
-       {
+        {
             cout << "esc key is pressed by user" << endl;
             break; 
-       }
+        }
     }
 
-   return 0;
+    return 0;
 
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
