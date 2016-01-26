56e0b10e9bf88e1e555756e3006e910a824cc7e3
Troy-Hinckley <troyhinckley@gmail.com>
2016-01-26 14:32:54 -0700

    working vision code

---
+++ vision/color_detection_test/build/CMakeFiles/ColorDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ColorDetection.dir/colordetection.cpp.o"
+  "ColorDetection.pdb"
+  "ColorDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ColorDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ vision/object_tracking_test/build/CMakeFiles/ObjectTracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ObjectTracking.dir/objecttracking.cpp.o"
+  "ObjectTracking.pdb"
+  "ObjectTracking"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ObjectTracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
