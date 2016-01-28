698abf718e90a5649d2716be8bb43a8ecba50db1
Troy Hinckley <t.macman@gmail.com>
2016-01-28 09:51:00 -0700

    Update objectdetection.h

--- vision/robot_vision/objectdetection.h
+++ vision/robot_vision/objectdetection.h
@@ -1,3 +1,16 @@
 
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include "utils.h"
+
+void CreateControlWindow(Range hue, Range saturation, Range value);
+
+VideoCapture ConnectCamera(char* uri = "");
+
+Mat ReadFrame(VideoCapture camera);//frame in HSV
+
+Mat ReadImage(char* = "");// image in HSV
+
+Mat SegmentColor(Mat img, HsvColorSubSpace colorSegment);
+
+
