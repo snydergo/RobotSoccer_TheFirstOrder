f1525cf9721f866f01beb8df1f3afdda28e01669
Glade <snydergo@gmail.com>
2016-02-01 12:43:21 -0700

    Contains data types of information that will be received from Vision. Package was included to also include the communication that will be happening via ROS

---
+++ AI/visiondata/GameStatus.py
@@ -0,0 +1,94 @@
+"""
+Contains the data received from Vision. It will have the Soccer ball and its location and velocity and predicted
+value, alongside with the robots and their locations along with any other important parameters.
+"""
+
+import array
+
+class VisionAI(Exception):
+    errorType = 0;
+    def __init__(self, errorType):
+        self.errorType = errorType
+    def __str__(self):
+        return "incorrect parameters passed in"
+
+class Point(object):
+    x = 0;
+    y = 0;
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "[%d,%d]\n" %(self.x,self.y)
+
+class FieldObject():
+    location = Point(0,0)
+    v = array(0,0)
+    def __init__(self, gvnLocation: Point, velocity: array) -> object:
+        self.location = gvnLocation
+        self.v = velocity
+
+    def __str__(self):
+        return "Point = [%d,%d] velocity = [%d,%d]\n" %(self.Point.x,self.Point.y, self.v[1], self.v[2])
+
+    def getVelArray(self):
+        return self.v
+
+    def getLocation(self):
+        return self.location
+
+    def setLocation(self, x, y):
+        location = Point(x,y)
+
+    def setVelocity(self, velocity: array):
+        v = velocity
+
+
+
+
+class GameStatus(object):
+    allyRobot1 = FieldObject(Point(0,0),array(0,0))
+    allyRobot2 = FieldObject(Point(0,0),array(0,0))
+    enemyRobot1 = FieldObject(Point(0,0),array(0,0))
+    enemyRobot2 = FieldObject(Point(0,0),array(0,0))
+    ball = FieldObject(Point(0,0),array(0,0))
+
+    def __init__(self, allyRobot1: FieldObject, allyRobot2: FieldObject, enemyRobot1: FieldObject, enemyRobot2: FieldObject, ball: FieldObject):
+        self.allyRobot1 = allyRobot1
+        self.allyRobot2 = allyRobot2
+        self.enemyRobot1 = enemyRobot1
+        self.enemyRobot2 = enemyRobot2
+        self.ball = ball
+
+    def getEnemyRobot1(self) -> FieldObject:
+        return self.enemyRobot1
+
+    def getEnemyRobot2(self) -> FieldObject:
+        return self.enemyRobot2
+
+    def getAllyRobot1(self) -> FieldObject:
+        return self.allyRobot1
+
+    def getAllyRobot2(self) -> FieldObject:
+        return self.allyRobot2
+
+    def getBall(self) -> FieldObject:
+        return self.ball
+
+    def setEnemyRobot1(self, enemyRobot1: FieldObject):
+        self.enemyRobot1 = enemyRobot1
+
+    def setEnemyRobot2(self, enemyRobot2: FieldObject):
+        self.enemyRobot2 = enemyRobot2
+
+    def setAllyRobot1(self, allyRobot1: FieldObject):
+        self.allyRobot1 = allyRobot1
+
+    def setAllyRobot2(self, allyRobot2: FieldObject):
+        self.allyRobot2 = allyRobot2
+
+    def setBall(self, ball: FieldObject):
+        self.ball = ball
+
+
