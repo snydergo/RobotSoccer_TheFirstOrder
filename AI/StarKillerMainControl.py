cc25d7a9082d7bf99db746413445e4d68f519446
Glade <snydergo@gmail.com>
2016-02-03 10:53:23 -0700

    add algorithm to go to center

--- AI/StarKillerMainControl.py
+++ AI/StarKillerMainControl.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """ StarKillerMainControl.py Contains the most abstract level of Program that controls the Robot and Contains
 all information of the Field. This is the main control of all First Order forces
 """
@@ -8,6 +9,7 @@ from motion_control.mlib import *
 
 #global that is updated by AI/visiondata/listener.py by vision and is updated periodically
 glob_gameStatus = object()
+glob_gameStatusUpdated = 0
 
 class StarKillerData(object):
     robots = FieldObject("robot",Point(0,0), Direction(0,0))
@@ -29,12 +31,30 @@ if __name__=="__main__":
     glob_gameStatus = GameStatus(ally1,ally2,enemy1,enemy2,ball)
     ##spin in circle
 
+
     ##go forward
-    
+
     ##go in square
 
     #go to the center
-    toCenter = directionToPoint(ally1.getLocation(), Point(0,0))
+    while(1):
+        if(glob_gameStatusUpdated):
+            toCenter = directionToPoint(glob_gameStatus.allyRobot1.getLocation(), Point(0,0))
+            vx_w = 0
+            vy_w = 0
+            #set the velocities to approach center
+            if(toCenter.x_dir < 3):
+                vx_w = -0.1
+            elif(toCenter.x_dir > 3):
+                vx_w = 0.1
+
+            if(toCenter.y_dir < 3):
+                vy_w = -0.1
+            elif(toCenter.y_dir > 3):
+                vy_w = 0.1
+
+            goXYOmegaWorld(vx_w,vy_w)
+
 
 
     #importer.importGameStatus(importer,"dataExample.json", deathstar)
