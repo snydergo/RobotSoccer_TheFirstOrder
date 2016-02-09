96c5e59d17cf03ba812395b0f63a8b7baf952ed7
Troy Hinckley <t.macman@gmail.com>
2016-02-09 09:34:25 -0700

    Update definedcvobject.h

--- vision/robot_vision/definedcvobject.h
+++ vision/robot_vision/definedcvobject.h
@@ -7,6 +7,4 @@ class DefinedCVObject
 public:
   virtual bool update(std::vector<UndefinedCVObject> cvObjs){return false;}
   virtual bool find(std::vector<UndefinedCVObject> cvObjs){return false;}
-  
-  cv::Point2f center;
 };
