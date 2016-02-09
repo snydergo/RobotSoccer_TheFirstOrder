888b7c59cf8a9aa81d355c4c0bdac48da82f68dd
Troy Hinckley <t.macman@gmail.com>
2016-02-09 09:30:42 -0700

    Update robot.h

--- vision/robot_vision/robot.h
+++ vision/robot_vision/robot.h
@@ -1,19 +1,18 @@
 #pragma once
 
 #include "undefinedcvobject.h"
+#include "definedcvobject.h"
 
-class Robot
+class Robot: public DefinedCVObject
 {
 public:
   
-  bool update(std::vector<UndefinedCVObject> cvObjs);
-  bool find(std::vector<UndefinedCVObject> cvObjs);
+  bool update(std::vector<UndefinedCVObject> cvObjs) override;
+  bool find(std::vector<UndefinedCVObject> cvObjs) override;
   
 private:
   UndefinedCVObject front;
   UndefinedCVObject rear;
-  
-  cv::Point2f center;
-  float theta;
 
+  float theta;
 };
