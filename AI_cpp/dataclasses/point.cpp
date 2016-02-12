fbaee9baaacf1ec01b6dadd3f7267769acb3bd44
Troy Hinckley <t.macman@gmail.com>
2016-02-11 23:36:59 -0700

    Update point.cpp

--- AI_cpp/dataclasses/point.cpp
+++ AI_cpp/dataclasses/point.cpp
@@ -1,15 +1,5 @@
 #include "point.h"
 
-Point::Point(){
-    this->x = 0;
-    this->y = 0;
-}
-
-Point::Point(double x, double y){
-    this->x = x;
-    this->y = y;
-}
-
 std::string Point::toString(){
     return "null";
 }
