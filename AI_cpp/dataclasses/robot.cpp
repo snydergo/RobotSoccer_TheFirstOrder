ad6832e887763efd92c3e307ff59f1c172414239
Troy Hinckley <t.macman@gmail.com>
2016-02-11 23:41:26 -0700

    Update robot.cpp

--- AI_cpp/dataclasses/robot.cpp
+++ AI_cpp/dataclasses/robot.cpp
@@ -1,12 +1,5 @@
 #include "robot.h"
 
-//Robot Object functions
-Robot::Robot(std::string tag, Point loc, Point vel, double theta, robotType type)
-    : FieldObject(tag, loc, vel){
-    this->theta = theta;
-    this->utils = new Utilities(tag, type);
-}
-
 std::string Robot::toString(){
     return "null";
 }
