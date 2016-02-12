de26237ca0c13a1ff1b3ee10be63500136d10a19
Troy Hinckley <t.macman@gmail.com>
2016-02-11 23:47:54 -0700

    Update utilities.cpp

--- AI_cpp/gameplay/utilities.cpp
+++ AI_cpp/gameplay/utilities.cpp
@@ -2,11 +2,6 @@
 
 #include "../bookkeeping.h"
 
-Utilities::Utilities(std::string gvnName, robotType num){
-    this->robotNum = num;
-    this->name = gvnName;
-}
-
 void Utilities::rotate(double angle){
     printf("send command to move angle %f", angle);
 }
@@ -23,7 +18,7 @@ void Utilities::move(Robot robot, Point dir, double des_theta){
 
 void Utilities::moveToPoint(Robot robot, Point point, double theta){
     Point dir = calc::directionToPoint(robot.location, point);
-    this->move(robot, dir, theta);
+    move(robot, dir, theta);
 }
 
 void Utilities::kick(double power, double theta){
@@ -33,12 +28,12 @@ void Utilities::kick(double power, double theta){
 
 void Utilities::moveToCenter(Robot robot, double theta){
     Point movVector = calc::directionToPoint(robot.location, center);
-    this->move(robot, movVector, theta);
+    move(robot, movVector, theta);
 }
 
 void Utilities::followBall(FieldObject ball, Robot robot, double theta){
     printf("following ball\n");
     Point movVector = calc::directionToPoint(robot.location, ball.location);
-    this->move(robot, movVector, theta);
+    move(robot, movVector, theta);
 }
 
