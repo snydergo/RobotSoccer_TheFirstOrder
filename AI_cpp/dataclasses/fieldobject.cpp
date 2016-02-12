c51782b289eb4750b0344a82e4a44a2b2a89560c
Troy Hinckley <t.macman@gmail.com>
2016-02-11 23:23:05 -0700

    Update fieldobject.cpp

--- AI_cpp/dataclasses/fieldobject.cpp
+++ AI_cpp/dataclasses/fieldobject.cpp
@@ -1,12 +1,5 @@
 #include "fieldobject.h"
 
-//Field Object Functions
-FieldObject::FieldObject(std::string tag, Point loc, Point vel){
-    this->tag = tag;
-    this->location = loc;
-    this->velocity = vel;
-}
-
 std::string FieldObject::toString(){
     return "null";
 }
