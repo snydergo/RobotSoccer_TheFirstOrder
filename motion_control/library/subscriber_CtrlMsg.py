d5fa386fd0225e78e1071851496b95f20b48257f
Matthew Kirk <matthewkirk203@gmail.com>
2016-02-13 14:14:24 -0700

    got rid of callback2

--- motion_control/library/subscriber_CtrlMsg.py
+++ motion_control/library/subscriber_CtrlMsg.py
@@ -17,10 +17,6 @@ def callback1(data):
     # print data
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def callback2(data):
-    print data
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
     
 def ControlListener1():
 
@@ -42,20 +38,6 @@ def ControlListener1():
 
         rospy.spinOnce()
         return
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
-def ControlListener2():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('robot2', anonymous=True)
-
-    rospy.Subscriber("robot2Com", controldata, callback2)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
