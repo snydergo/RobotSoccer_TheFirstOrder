31deeecaff28e46dc3fd5033c925ddde16ea2ce6
Matthew Kirk <matthewkirk203@gmail.com>
2016-02-13 12:35:07 -0700

    added speed control

--- motion_control/library/manual.py
+++ motion_control/library/manual.py
@@ -22,26 +22,39 @@ def get_action():
         return 'SPIN_CW'
     elif pressed == ' ':
         return 'STOP'
+    elif pressed == ',':
+        return 'SPEED_DEC'
+    elif pressed == '.'
+        return 'SPEED_INC'
     elif pressed == 'k':
         return 'KILL'
 
 def main():
+    #default speed
+    speed = .1
+
     while 1:
         action = get_action()
         if action == 'UP':
-            mlib.goXYOmegaWorld(.1,0)
+            mlib.goXYOmegaWorld(speed,0)
         elif action == 'LEFT':
-            mlib.goXYOmegaWorld(0,.1)
+            mlib.goXYOmegaWorld(0,speed)
         elif action == 'DOWN':
-            mlib.goXYOmegaWorld(-.1,0)
+            mlib.goXYOmegaWorld(-speed,0)
         elif action == 'RIGHT':
-            mlib.goXYOmegaWorld(0,-.1)
+            mlib.goXYOmegaWorld(0,-speed)
         elif action == 'SPIN_CCW':
             mlib.goXYOmegaWorld(0,0,1)
         elif action == 'SPIN_CW':
             mlib.goXYOmegaWorld(0,0,-1)
         elif action == 'STOP':
             mlib.stop()
+        elif action == 'SPEED_DEC'
+            speed = speed - .1
+            print("Speed: ",speed)
+        elif action == 'SPEED_INC'
+            speed = speed + .1
+            print("Speed: ",speed)
         elif action == 'KILL':
             return
 
