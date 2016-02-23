8a61441ffdeac4283e889988fe886afab81794fb
Troy Hinckley <t.macman@gmail.com>
2016-02-23 12:22:09 -0700

    Update config.h

--- vision/robot_vision/config.h
+++ vision/robot_vision/config.h
@@ -34,6 +34,16 @@ extern int opponentRobotSmallArea;
 
 extern std::string cameraUrl;
 
+const HsvColorSubSpace green({60,90},{30,145},{180,255});
+const HsvColorSubSpace blue({80,100},{30,235},{170,255});
+const HsvColorSubSpace purple({120,140},{30,90},{200,250});
+const HsvColorSubSpace red({175,179},{30,200},{200,255});
+const HsvColorSubSpace orange({0,30},{50,190},{150,255});
+const HsvColorSubSpace green({140,175},{30,150},{120,255});
+const HsvColorSubSpace yellow({10,45},{0,90},{225,255});
+
+
+
 
 } // namespace config
 
