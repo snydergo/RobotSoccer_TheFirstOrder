31962b46696ea7017de572574fd0e722d102d6e8
Clone <snydergo@gmail.com>
2016-02-23 11:12:14 -0700

    Update bookkeeping.cpp

--- AI_cpp/bookkeeping.cpp
+++ AI_cpp/bookkeeping.cpp
@@ -1,25 +1,11 @@
 #include "bookkeeping.h"
 
-bool pend(sem semaphore) {
-    if(semaphore){
-        semaphore--;
-        return true;
-    }else
-        return false;
-}
-
-void post(sem semaphore) {
-    semaphore++;
-}
-
 Point center = Point(0,0);
 Point enemyGoal = Point(GOAL_XLOCATION,0);
 Point allyGoal = Point(-GOAL_XLOCATION,0);
 Point start1Location = Point(-GOAL_XLOCATION,0);
 FieldCoord field;
 bool visionUpdated;
-sem visionMsg_sem = 1;
-sem ctrlMsg_sem = 1;
 GameStatus visionStatus_msg;
 
 bool sendCmd_Rob1 = true;
