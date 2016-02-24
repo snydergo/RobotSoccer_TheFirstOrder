4727519f076e772ebbf656ae84af441624d51e16
James Parker <parkerpatriot@gmail.com>
2016-02-23 20:40:59 -0700

    troubleshooting pid

--- motion_control/library/sample_pid.py
+++ motion_control/library/sample_pid.py
@@ -7,15 +7,15 @@ class ControlVar:
 
 class Param:
     def __init__(self):
-        self.kp_x   = .01
+        self.kp_x   = .1
         self.ki_x   = .001
         self.kd_x   = 0 #.002
 
-        self.kp_y   = .01
+        self.kp_y   = .1
         self.ki_y   = .001
         self.kd_y   = 0 #.002
 
-        self.kp_th  = .001
+        self.kp_th  = .01
         self.ki_th  = .0000
         self.kd_th  = 0
 
