6cc66b668fc343b0381eefb33431874249f6010a
Clone <snydergo@gmail.com>
2016-02-25 19:49:25 -0700

    Update types.h

--- AI_cpp/types.h
+++ AI_cpp/types.h
@@ -1,12 +1,15 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+//## FIELD PARAMETERS ##//
 #define GOAL_XLOCATION 160
 #define GOAL_WIDTH 60
 #define GOAL_RADIUS GOAL_WIDTH/2
 #define FIELD_XBORDER 180
 #define FIELD_YBORDER 88
 #define PI 3.141592653
+
+//## THRESHOLD DEFINITIONS ##//
 #define ANGLE_ERR 10
 #define DISTANCE_ERR 16 // this is an error of 4cm
 #define PERIMETER_ERR 124
@@ -14,10 +17,12 @@
 #define BALLFETCHED_ERR 20
 #define FETCHBALL_OFFSET 20
 
+//## TIMING DEFINES ##//
 #define TICKS_PER_SEC 50
-
 #define VELOCITY_THRESHOLD .000001
 #define VELOCITY_DIVIDER (1/TICKS_PER_SEC)
+
+
 //# FILTER DEFINES #//
 #define DOSAMPLE    0
 #define BETASMOOTH0 .5 //main filter
