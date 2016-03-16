38e4f520ce7e8b998a13ad5301655d2ccf7d1922
James Parker <parkerpatriot@gmail.com>
2016-03-16 07:57:55 -0600

    working PID values

--- ros_ws/src/robot_soccer/motion_control/library/globals.py
+++ ros_ws/src/robot_soccer/motion_control/library/globals.py
@@ -3,18 +3,18 @@ from numpy import matlib
 
 # Troy's values
 
-kp_x	 = 0.008
+kp_x	 = 0.009
 ki_x	 = 0#0.0001
 kd_x	 = 0.0
 kp_y	 = kp_x
 ki_y	 = ki_x
 kd_y	 = kd_x
-kp_theta = .008#0.013
+kp_theta = .009#0.013
 ki_theta = 0.00
 kd_theta = 0.00
 
-xy_limit	 = 2.0#.4
-th_limit	 = 2.0#0.2 #5
+xy_limit	 = 1.0#.4
+th_limit	 = 1.0#0.2 #5
 
 xy_thresh	 = 100#5
 theta_thresh = 100#5
