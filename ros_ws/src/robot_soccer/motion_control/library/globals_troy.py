549ae543acbb9657a6e272beee9068e2abd3acb5
Troy Hinckley <t.macman@gmail.com>
2016-03-29 10:31:26 -0600

    Update globals_troy.py

--- ros_ws/src/robot_soccer/motion_control/library/globals_troy.py
+++ ros_ws/src/robot_soccer/motion_control/library/globals_troy.py
@@ -1,29 +1,36 @@
 import numpy as np
 from numpy import matlib
 
-kp_x	 = 0.008
-ki_x	 = 0#0.0001
-kd_x	 = 0.0
+kp_x	 = 0.02
+ki_x	 = 0
+kd_x	 = 0.001
 kp_y	 = kp_x
 ki_y	 = ki_x
 kd_y	 = kd_x
-kp_theta = .008#0.013
-ki_theta = 0.00
-kd_theta = 0.00
+kp_theta = 0.013
+ki_theta = 0
+kd_theta = 0.005
 
-xy_limit	 = 1.0#.4
-th_limit	 = 50#0.2 #5
+fast_limit = .7
+slow_limit = .3
+xy_limit	 = slow_limit#0.7#.4
+th_limit	 = slow_limit#0.2 #5
 
-xy_thresh	 = 0#5
-theta_thresh = 5#5
+xy_thresh	 = 5#5
+theta_thresh = 100#5
 
-loop_rate = 20 # Hz (get value from publisher)
-Ts     = 1.00/loop_rate
-tau    = 0.005
-camera_sample_rate = 33
-lpf_alpha          = 0.7 
-a1      = (2*tau-camera_sample_rate)/(2*tau-tau+camera_sample_rate)
-a2      = 2/(2*tau+camera_sample_rate)
+loop_rate 			= 60.0 # Hz (get value from publisher)
+loop_time			= 1.0/loop_rate
+Ts    			 	= 1.0/loop_rate
+
+camera_sample_rate 	= 30.0 # Hz
+camera_sample_time = 1.0/camera_sample_rate
+
+lpf_alpha          	= 0.7
+
+tau    				= camera_sample_time/5 #0.005
+a1 					= (2*tau-camera_sample_time)/(2*tau+camera_sample_time)
+a2 					= 2/(2*tau+camera_sample_time)
 
 # field characteristics
 field_length  = 3.048; # meters (5 ft)
@@ -36,10 +43,19 @@ S = np.diag([\
     (5*np.pi/180)**2,               # initial variance of angle of ownteam i 
     ]);
 
-camera_sigma_robot_position = 2
-camera_sigma_robot_position = 3
-camera_sigma_robot_angle = np.pi/9
+# noise levels on the camera
+camera_sigma_ball = 0.01 # units are meters
+camera_sigma_robot_position = 0.01 # units are meters
+camera_sigma_robot_angle = 2*np.pi/180 # units are radians
 
+
+
+# parameters for the Kalman filter
 Q_ownteam = np.diag([1**2,1**2,(2*np.pi/180)**2])
 R_ownteam = np.diag([camera_sigma_robot_position**2, camera_sigma_robot_position**2, camera_sigma_robot_angle**2])
+Q_opponent = 1*np.diag([.001**2, .001**2, .01**2, 0.1**2, 0.1**2, 0.1**2, 1**2,1**2,(2*np.pi/180)**2])
+R_opponent = np.diag([camera_sigma_robot_position**2, camera_sigma_robot_position**2, camera_sigma_robot_angle**2])
+Q_ball = 1*np.diag([.001**2,.001**2,.01**2,.01**2, .1**2, .1**2, 10**2, 10**2])
+R_ball = np.diag([camera_sigma_ball**2, camera_sigma_ball**2])
+
 
